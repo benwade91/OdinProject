751fb635830bdb4e680597a2e0dcbcc14182c984
benwade91 <70988204+benn925@users.noreply.github.com>
2021-12-22 13:19:07 -0800

    changed structure to play game in web browser and added turn system

--- RPC/script.js
+++ RPC/script.js
@@ -1,25 +1,32 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
 const options = ['ROCK', 'PAPER', 'SCISSORS'];
 
-rl.question("ROCK, PAPER or SCISSORS?\n", function(rawUserMove) {
+let userScore = 0
+let gameTurn = 1
+
+function turn() {
+    let rawUserMove = prompt('Rock, paper or scissors?')
     const computerMove = options[Math.floor(Math.random() * options.length)]
     userMove = rawUserMove.toUpperCase()
+    if(!options.includes(userMove)){return "Rock paper or scissors only!"}
     if (userMove === computerMove) {
-        console.log('Its a tie!');
+        return('Its a tie!');
     } else if (userMove === 'ROCK' && computerMove === "PAPER") {
-        console.log('Computer chose PAPER! You LOSE');
+        return('Computer chose PAPER! You LOSE');
     } else if (userMove === 'PAPER' && computerMove === "SCISSORS") {
-        console.log('Computer chose SCISSORS! You LOSE');
+        return('Computer chose SCISSORS! You LOSE');
     } else if (userMove === 'SCISSORS' && computerMove === "ROCK") {
-        console.log('Computer chose ROCK! You LOSE');
+        return('Computer chose ROCK! You LOSE');
     } else {
-        console.log("You Won!");
+        userScore += 1
+        return("You Won!");
     }
-    console.log(userMove, computerMove);
+}
 
-        rl.close();
-});
+
+for(let i = 0; i < 5; i++){
+    let result = turn()
+    alert(result);
+}
+
+alert(`Your final score is ${userScore}`)
