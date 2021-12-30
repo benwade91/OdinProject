a752a7fad92019611f761c41105df4eb8927b6c7
benwade91 <70988204+benn925@users.noreply.github.com>
2021-12-30 14:23:12 -0800

    added scoreboard and updated gameplay for RPC

--- RPC/script.js
+++ RPC/script.js
@@ -1,49 +1,56 @@
-const options = ['ROCK', 'PAPER', 'SCISSORS'];
+const options = ['rock', 'paper', 'scissors'];
 
 let userScore = 0
-let gameTurn = 1
+let gameTurn = 5
 
-function turn(rawUserMove) {
+function turn(userMove) {
     const computerMove = options[Math.floor(Math.random() * options.length)]
-    userMove = rawUserMove.toUpperCase()
-    if (!options.includes(userMove)) {
-        return "Rock paper or scissors only!"
-    }
+    let message = ''
     if (userMove === computerMove) {
-        return ('Its a tie!');
-    } else if (userMove === 'ROCK' && computerMove === "PAPER") {
-        return ('Computer chose PAPER! You LOSE');
-    } else if (userMove === 'PAPER' && computerMove === "SCISSORS") {
-        return ('Computer chose SCISSORS! You LOSE');
-    } else if (userMove === 'SCISSORS' && computerMove === "ROCK") {
-        return ('Computer chose ROCK! You LOSE');
+        message = 'Its a tie!';
+    } else if (userMove === 'rock' && computerMove === "paper") {
+        message = 'Computer chose PAPER! You LOSE';
+    } else if (userMove === 'paper' && computerMove === "scissors") {
+        message = 'Computer chose SCISSORS! You LOSE';
+    } else if (userMove === 'scissors' && computerMove === "rock") {
+        message = 'Computer chose ROCK! You LOSE';
     } else {
         userScore += 1
-        return ("You Won!");
+        message = "You Won!";
     }
+    gameTurn--;
+    updateScore(message, userScore)
 }
 
 
-const game = (turns) => {
-    for (let i = 0; i < turns; i++) {
-        let result = turn()
-        alert(result);
+const updateScore = (message, score) => {
+    document.querySelector('.scoreBoard').innerHTML = `<h1>Score: ${score}</h1><h1>Turns: ${gameTurn}</h1>`
+    document.querySelector('.gameMessage').innerHTML = `<h4>${message}</h4>`
+    if (gameTurn === 0) {
+        gameSelections.forEach((button) => {
+            button.classList.add('none')
+        });
+        document.querySelector('.scoreBoard').innerHTML = `<h1>Final Score: ${score}</h1>`
     }
-    alert(`Your final score is ${userScore}`)
 }
+
+const startGame = () => {
+    startBtn.classList.add('none')
+    gameSelections.forEach((button) => {
+        button.classList.remove('none')
+    })
+    document.querySelector('.scoreBoard').classList.remove('none')
+}
+
 const startBtn = document.querySelector('#start');
 const gameSelections = document.querySelectorAll('.gameImg');
 
 gameSelections.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(turn(button.id));
+        turn(button.id);
     });
 });
 
 startBtn.addEventListener('click', () => {
-    console.log('clicked');
-    gameSelections.forEach((button)=>{
-        button.classList.remove('none')
-    })
+    startGame()
 })
-// game(5)
